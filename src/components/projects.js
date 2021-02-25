96e7efcbcdedd90b4805b6610ee8468d24ece491
gharsalliyasser <71494140+gharsalliyasser@users.noreply.github.com>
2021-02-25 22:51:08 +0100

    r

---
+++ src/components/projects.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+export default class Projects extends Component {
+  render() {
+    let Projects = this.props.resumeData;
+    return (
+      <section id="portfolio">
+                         <h1>devchallenges buttons component REACT</h1>
+                         <img className="profile-pic"  src="images/btn.png" alt="" />
+                         {/* <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
+
+                         <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
+
+                         <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
+                         
+
+                         <img className="profile-pic"  src="iimages/profile.jpg" alt="" />
+                         
+                         <img className="prfile-pc" src="images/profile.jpg" alt=" " /> */}
+        
+      </section>
+    );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,45 @@
+let resumeData = {
+    "name": "YASSER GHARSALLI",
+    "role": "Frontend Developer ",
+    "linkedinId":"gharsalliyasser",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/gharsalli-yasser-2387051bb/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/gharsalliyasser",
+          "className":"fa fa-github"
+        },
+        
+      ],
+    "aboutme":"I am fresh graduated student at RBK TUNISIA ",
+    "address":"tunisia",
+    
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Vue"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"angular"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ],
+  }
+  
+  export default resumeData
