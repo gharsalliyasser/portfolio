66ab7b496ad63d63310733fce6a5d915f79e4217
gharsalliyasser <71494140+gharsalliyasser@users.noreply.github.com>
2021-03-14 13:34:03 +0100

    y

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,8 +11,10 @@ export default class Header extends Component {
                <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
                <li><a className="smoothscroll" href="#about">About</a></li>
              <li><a className="smoothscroll" href="#resume">Resume</a></li>
+               <li><a className="smoothscroll" href="#Projects">projects</a></li>
                <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
                <li><a className="smoothscroll" href="#contact">Contact</a></li>
+
             </ul>
          </nav>
 
--- src/components/projects.js
+++ src/components/projects.js
@@ -3,10 +3,10 @@ export default class Projects extends Component {
   render() {
     let Projects = this.props.resumeData;
     return (
-      <section id="portfolio">
+      <section id="Projects">
                          <h1>devchallenges buttons component REACT</h1>
                          <img className="profile-pic"  src="images/btn.png" alt="" />
-                         {/* <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
+                       {/* <img className="profile-pic"  src="images/profilepic.jpg" alt="" /> 
 
                          <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
 
@@ -15,7 +15,7 @@ export default class Projects extends Component {
 
                          <img className="profile-pic"  src="iimages/profile.jpg" alt="" />
                          
-                         <img className="prfile-pc" src="images/profile.jpg" alt=" " /> */}
+                         <img className="prfile-pc" src="images/profile.jpg" alt=" " />  */}
         
       </section>
     );
